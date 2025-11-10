78dc22267c05212b1925e7920dcf742061d57b3f
johlopezs-art <joh.lopezs@duocuc.cl>
2025-11-10 19:51:11 -0300

    johan v0.0.2
    error404

--- src/app/pages/error404/error404.page.ts
+++ src/app/pages/error404/error404.page.ts
@@ -1,22 +1,26 @@
-// src/app/pages/error404/error404.page.ts
-
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { RouterLink } from '@angular/router'; // ⬅️ Necesario para usar routerLink en el botón
+import { RouterLink } from '@angular/router';
+import { LottieComponent, AnimationOptions } from 'ngx-lottie'; 
 
 @Component({
   selector: 'app-error404',
-  templateUrl: './error404.page.html', // ⬅️ Apunta al archivo HTML
+  templateUrl: './error404.page.html',
   styleUrls: ['./error404.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule, RouterLink] // ⬅️ RouterLink incluido en imports
+  imports: [CommonModule, IonicModule, RouterLink, LottieComponent], 
 })
-export class Error404Page implements OnInit { // ⬅️ Nombre de la clase confirmado
+export class Error404Page implements OnInit {
 
-  constructor() { }
+  
+  options: AnimationOptions = {
+    path: 'assets/animation/error404.json', 
+    loop: true,
+    autoplay: true
+  };
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
