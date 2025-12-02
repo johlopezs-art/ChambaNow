366e09c9649553429301035073de478e24282aae
johlopezs-art <joh.lopezs@duocuc.cl>
2025-12-01 22:41:12 -0300

    johan V1.1.4
    arreglo error404

--- src/app/pages/error404/error404.page.ts
+++ src/app/pages/error404/error404.page.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+// Importaciones explícitas de componentes Ionic Standalone
+import { 
+  IonContent, 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonNote, 
+  IonButton 
+} from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
-import { LottieComponent, AnimationOptions } from 'ngx-lottie'; 
 
 @Component({
   selector: 'app-error404',
   templateUrl: './error404.page.html',
   styleUrls: ['./error404.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule, RouterLink, LottieComponent], 
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Necesario para usar <lottie-player>
+  imports: [
+    CommonModule, 
+    RouterLink,
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonNote,
+    IonButton
+  ], 
 })
 export class Error404Page implements OnInit {
-
-  
-  options: AnimationOptions = {
-    path: 'assets/animation/error404.json', 
-    loop: true,
-    autoplay: true
-  };
 
   constructor() {}
 
